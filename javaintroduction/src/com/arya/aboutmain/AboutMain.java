7793fc54c5c2b6a80a6b0f15ba816fb0655af2ec
user <user@192.168.1.27>
2024-01-23 19:51:47 +0530

    this is my

---
+++ javaintroduction/src/com/arya/aboutmain/AboutMain.java
@@ -0,0 +1,28 @@
+package com.arya.aboutmain;
+
+public class AboutMain {
+	public static void main(String args[]) {
+		System.out.println("vishal");
+	}
+	//main method not found
+	//private static void main(String args[]){
+//	System.out.println("hiiii");
+//	}
+	// main method is not static 
+//	private void main(String args[]){
+//	System.out.println("ranmms")
+//}
+	
+	
+//	Syntax error on token "@", delete this token
+//
+//	public static void main(String args[]) {
+//		int @a=5;
+//		System.out.println(a);
+//	}
+	
+//	
+	
+	
+	
+}
